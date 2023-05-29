3bcef330feeac65b73de24fdf86729c6525763e5
mrdinall <danikardinal@yahoo.com>
2023-05-30 00:43:52 +0700

    first app-obvan with template sb-admins

---
+++ app/Views/home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to CodeIgniter 4!</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+<!-- HEADER: MENU + HEROE SECTION -->
+<header>
+
+<h1>Test App Obvan</h1>
+
+
+</header>
+
+<!-- CONTENT -->
+
+<!-- start section -->
+<section>
+
+
+</section>
+<!-- end section -->
+<!-- start footer -->
+<footer>
+
+
+</footer>
+<!-- end footer -->
+
+<!-- SCRIPTS -->
+
+
+
+<!--start javascript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="js/app.js"></script>
+<!-- end javascript -->
+
+</body>
+</html>
